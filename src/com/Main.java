8e0595c67142a4972e14257e8cbbc9a55f636df3
mar4enko <marchenkopavel2006@gmail.com>
2022-03-21 12:46:37 +0200

    Task 61 is done

--- src/com/Main.java
+++ src/com/Main.java
@@ -36,10 +36,50 @@ public class Main {
      }
     }
 
+    /**
+     *Составить программу возведения натурального числа в квадрат, учитывая следующую закономерность:
+     * 1^2 = 1
+     * 2^2 = 1 + 3
+     * 3^2 = 1 + 3 + 5
+     * 4^2 = 1 + 3 + 5 + 7
+     * …
+     * n^2 = 1 + 3 + 5 + 7 + 9 + ... + 2n – 1
+     */
+    public static void Task61(){
+    int n = 5;
+    int num = 0;
+    for (int i=1;i<=n;i++){
+        num += 2*i-1;
+    }
+        System.out.println("Answer: " + num);
+    }
+    /**
+     *
+     */
+    public static void Task86(){
+
+    }
+    /**
+     *
+     */
+    public static void Task111(){
+
+    }
+    /**
+     *
+     */
+    public static void Task136(){
+
+    }
+
 
     public static void main(String[] args) {
-        //Variant();
-        Task36();
+       // Variant();
+       // Task36();
+        Task61();
+        Task86();
+        Task111();
+        Task136();
     }
 
 }
